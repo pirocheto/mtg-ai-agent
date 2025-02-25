6758fbc6004c98dba75490af2ae527fba9944735
pirocheto <pirocheto@gmail.com>
2025-02-25 16:21:00 +0100

    update ci

--- utils/create_env.sh
+++ utils/create_env.sh
@@ -1,7 +1,5 @@
 # This script creates a .env file with the necessary environment variables for the application to run.
 # It use the Scaleway CLI to access the secrets stored in the Scaleway Secret Manager.
-
-APP_NAME='mtg-ai-agent'
 EMAIL=$(scw secret version access-by-path secret-name=email secret-path=/ revision=1 | grep Data | awk '{print $2}' | base64 -d)
 CHAINLIT_AUTH_SECRET=$(scw secret version access-by-path secret-name=chainlit-auth-secret secret-path=/ revision=1 | grep Data | awk '{print $2}' | base64 -d)
 OPENAI_API_KEY=$(scw secret version access-by-path secret-name=openai-api-key secret-path=/ revision=1 | grep Data | awk '{print $2}' | base64 -d)
