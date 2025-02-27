9346bae8cefc297e03077c20647fe70ef06519db
pirocheto <pirocheto@gmail.com>
2025-02-27 15:32:22 +0100

    set openai model

--- app/agent/config.py
+++ app/agent/config.py
@@ -1,6 +1,27 @@
-from langchain.chat_models import init_chat_model
+from functools import lru_cache
 
-model = init_chat_model(
-    "openai:gpt-4o",
-    # "deepseek:deepseek-reasoner",
-)
+from langchain_core.language_models import BaseChatModel
+
+
+@lru_cache
+def get_model() -> BaseChatModel:
+    from langchain.chat_models import init_chat_model
+
+    return init_chat_model(
+        "openai:gpt-4o",
+        # Deepseek is unstable for function calling
+        # See https://api-docs.deepseek.com/guides/function_calling
+        # "deepseek:deepseek-reasoner",
+        # "deepseek:deepseek-chat",
+    )
+
+    # Config for model from Scaleway
+    # from langchain_openai import ChatOpenAI
+    # return ChatOpenAI(
+    #     model="mistral-nemo-instruct-2407",
+    #     base_url="https://api.scaleway.ai/v1",
+    #     api_key=os.environ["SCW_SECRET_KEY"],
+    # )
+
+
+model = get_model()
